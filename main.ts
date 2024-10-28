6b7bc54698a3b48397006f5107f79eec508aaa21
y_krenta <krenta.yv@aobfg.ru>
2024-10-28 13:23:00 +0300

    1 урок, инициализация Playwright

--- main.ts
+++ main.ts
@@ -1,2 +1,6 @@
-alert("Hello World");
-console.log(100);
+function sendMessage({arg1, arg2}: { arg1: any, arg2: any }) {
+    console.log(arg1)
+    console.log(arg2)
+}
+
+sendMessage({arg1: "Мам, привет", arg2: "да, я покушал"})
