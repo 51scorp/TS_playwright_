3636631e8ad58e5113e60da4af1e5d82f62f5c92
y_krenta <krenta.yv@aobfg.ru>
2024-11-11 12:37:51 +0300

    + 1 урок

---
+++ tests/lesson2_hover.spec.ts
@@ -0,0 +1,7 @@
+import {test, expect} from '@playwright/test';
+
+test('get image caption', async ({page}) => {
+  await page.goto('https://the-internet.herokuapp.com/hovers');
+  await page.locator(`.figure`).nth(1).hover();
+  await expect(page.locator(`.figcaption`).nth(1)).toBeVisible();
+});
