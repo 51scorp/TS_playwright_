a0f5313b9e35a4471846158cf47ecfcb2c196597
Yuriy_K <krentawork@gmail.com>
2024-11-30 00:20:24 +0300

    new tests stepik

--- tests/lesson2_fill.spec.spec.ts
+++ tests/lesson2_fill.spec.spec.ts
@@ -4,18 +4,14 @@ const BASE_URL = 'http://uitestingplayground.com/sampleapp';
 const login = 'login';
 const password = 'pwd';
 
-test('lesson2_fill.spec', async ({page}) => {
-  await page.goto(BASE_URL);
-  const loginInput = await page.locator('[name="UserName"]');
-  const passwordInput = await page.locator(`[name="Password"]`);
-  const loginButton = await page.locator('#login');
-
+test('lesson2_fill.spec', async ({page: {goto, locator, waitForTimeout}}) => {
+  await goto(BASE_URL);
+  const [loginInput, passwordInput, loginButton] = await Promise.all([locator('[name="UserName"]'), locator(`[name="Password"]`), locator('#login')]);
   await expect(loginInput && passwordInput && loginButton).toBeVisible();
-
   await loginInput.fill(login);
   await passwordInput.fill(String(password));
   await loginButton.click();
-  await page.waitForTimeout(1000);
-  const linkElement = await page.locator('#loginstatus');
+  await waitForTimeout(1000);
+  const [linkElement] = await Promise.all([locator('#loginstatus')]);
   await expect(linkElement).toHaveText(`Welcome, ${login}!`);
 });
